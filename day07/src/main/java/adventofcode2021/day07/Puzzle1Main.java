ce0b623f4e4123ae137e2b40804965f5ff4e97fa
Alwaysgone <ffxiizocker@hotmail.com>
2021-12-07 18:55:00 +0100

    added day07 solutions

---
+++ day07/src/main/java/adventofcode2021/day07/Puzzle1Main.java
@@ -0,0 +1,68 @@
+package adventofcode2021.day07;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Puzzle1Main {
+
+	public static void main(String[] args) throws IOException {
+		try(BufferedReader reader = new BufferedReader(new FileReader(Paths.get(".").toAbsolutePath().resolve("puzzle7_input.txt").toFile()))) {
+			AtomicInteger min = new AtomicInteger(Integer.MAX_VALUE);
+			AtomicInteger max = new AtomicInteger(Integer.MIN_VALUE);
+			int[] crabs = Arrays.stream(reader.readLine().split(","))
+					.mapToInt(Integer::parseInt)
+					.sorted()
+					.toArray();
+			Arrays.stream(crabs)
+			.forEach(v -> {
+				if(v < min.get()) {
+					min.set(v);
+				} else if(v > max.get()) {
+					max.set(v);
+				}
+			});
+			int fuelRequired = Arrays.stream(crabs)
+					.map(v -> v - min.get())
+					.sum();
+			int currentLevel = min.get();
+			int crabIndex = 0;
+			for(int i = min.get(); i < max.get(); i++) {
+				while(currentLevel == crabs[crabIndex]) {
+					crabIndex++;
+					continue;
+				}
+				currentLevel++;
+				int oldFuelRequired = fuelRequired;
+				fuelRequired += crabIndex;
+				fuelRequired -= crabs.length - crabIndex;
+				if(fuelRequired > oldFuelRequired) {
+					fuelRequired = oldFuelRequired;
+					break;
+				}
+			}
+//			int currentLowest = Integer.MAX_VALUE;
+//			for(int i = min.get(); i < max.get(); i++) {
+//				int fuelRequirement = 0;
+//				for(int j = 0; j < crabs.length; j++) {
+//					fuelRequirement += Math.abs(i - crabs[j]);
+//				}
+//				if(fuelRequirement < currentLowest) {
+//					currentLowest = fuelRequirement;
+//				}
+//			}
+
+			// currentLevel 308
+			// crabIndex 502
+			
+			// 340056
+			
+			System.out.println(fuelRequired);
+			
+//			System.out.println(currentLowest);
+		}
+	}
+}
---
+++ day07/src/main/java/adventofcode2021/day07/Puzzle2Main.java
@@ -0,0 +1,45 @@
+package adventofcode2021.day07;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class Puzzle2Main {
+
+	public static void main(String[] args) throws IOException {
+		try(BufferedReader reader = new BufferedReader(new FileReader(Paths.get(".").toAbsolutePath().resolve("puzzle7_input.txt").toFile()))) {
+			AtomicInteger min = new AtomicInteger(Integer.MAX_VALUE);
+			AtomicInteger max = new AtomicInteger(Integer.MIN_VALUE);
+			int[] crabs = Arrays.stream(reader.readLine().split(","))
+					.mapToInt(Integer::parseInt)
+					.sorted()
+					.toArray();
+			Arrays.stream(crabs)
+			.forEach(v -> {
+				if(v < min.get()) {
+					min.set(v);
+				} else if(v > max.get()) {
+					max.set(v);
+				}
+			});
+			int currentLowest = Integer.MAX_VALUE;
+			for(int i = min.get(); i < max.get(); i++) {
+				int fuelRequirement = 0;
+				for(int j = 0; j < crabs.length; j++) {
+					int distance = Math.abs(i - crabs[j]);
+					fuelRequirement += IntStream.rangeClosed(1, distance)
+					.sum();
+				}
+				if(fuelRequirement < currentLowest) {
+					currentLowest = fuelRequirement;
+				}
+			}
+			
+			System.out.println(currentLowest);
+		}
+	}
+}
